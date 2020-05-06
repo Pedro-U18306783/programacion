bc976bbc6a73076698ee346fc93fddb19d18bce6
pedro7777777777 <U18306783@utp.edu.pe>
2020-05-05 19:15:12 -0500

    primer commit

---
+++ arearadiocirculo/src/arearadiocirculo/circulo.java
@@ -0,0 +1,33 @@
+
+package arearadiocirculo;
+
+import java.util.Scanner;
+
+public class circulo {
+
+	public static void main(String[] args) {
+		
+		
+		
+		//Pedir el radio de un circulo y calular su area A=PI*r2
+		
+		//calculamos variables
+		final double PI=3.14159;
+		double radio,areaCirculo;
+		//Ingreso
+		System.out.println("Ingrese el Radio :");
+		Scanner teclado =new Scanner(System.in);
+		radio=teclado.nextDouble();
+		//area del circulo
+		areaCirculo=PI*radio*radio;
+		//mostrar resultado
+		System.out.println("Area del Circulo es: "+areaCirculo);
+		
+		
+		
+		
+		
+
+	}
+
+}
